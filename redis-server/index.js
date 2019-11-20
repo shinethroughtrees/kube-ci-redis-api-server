1dcc3573c2ce2e7f5f060bf3c7bf91ee94b0078e
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2019-11-20 18:11:39 +0900

    pubsub, client made

--- redis-server/index.js
+++ redis-server/index.js
@@ -1,4 +1,6 @@
-var redis = require('redis');
+const express = require('express');
+const redis = require('redis');
+const app = express();
 const sub = redis.createClient({
     host: 'redis',
     port: 6379
@@ -7,21 +9,18 @@ const sub = redis.createClient({
     port: 6379
 });
 var msg_count = 0;
-
-sub.on("subscribe", function (channel, count) {
-    pub.publish("a nice channel", "I am sending a message.");
-    pub.publish("a nice channel", "I am sending a second message.");
-    pub.publish("a nice channel", "I am sending my last message.");
-});
+app.get('/', (req, res) => {
+    pub.publish("a nice channel", `I am sending ${msg_count}th message`);
+    res.send("hello world");
+} );
+app.post('/api/pub', )
 
 sub.on("message", function (channel, message) {
     console.log("sub channel " + channel + ": " + message);
     msg_count += 1;
-    if (msg_count === 3) {
-        sub.unsubscribe();
-        sub.quit();
-        pub.quit();
-    }
 });
 
 sub.subscribe("a nice channel");
+app.listen(8080,() => {
+    console.log('server started');
+})
